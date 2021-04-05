1db874fea9b3ecd1584e1829799ca50c25b8b4cf
Subeting <68261543+Subeting@users.noreply.github.com>
2021-04-05 16:17:43 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -2,6 +2,6 @@
 (apt update;apt install curl -y)||(yum install curl -y)
 curl https://get.docker.com|bash
 usermod -aG docker root;systemctl enable docker
-mkdir -p /etc/docker/;wget ## -O  /etc/docker/daemon.json
+mkdir -p /etc/docker/;wget https://cdn.jsdelivr.net/gh/Subeting/docker-onekey-install/daemon.json -O  /etc/docker/daemon.json
 systemctl daemon-reload
 systemctl restart docker
