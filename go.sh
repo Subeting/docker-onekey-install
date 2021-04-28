8a5a83c8d962bf59a6ca1bd48bba4d1d07298b36
Subeting <68261543+Subeting@users.noreply.github.com>
2021-04-28 22:46:09 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 (apt update;apt install curl -y)||(yum install curl -y)
+setenforce 0
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config  
 curl https://get.docker.com|bash
 usermod -aG docker root;systemctl enable docker
 mkdir -p /etc/docker/;wget https://cdn.jsdelivr.net/gh/Subeting/docker-onekey-install/daemon.json -O  /etc/docker/daemon.json
