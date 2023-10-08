9bc7c9c188363bf15f064910d0e78ac33f004f19
Subeting <68261543+Subeting@users.noreply.github.com>
2023-10-08 15:21:35 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -4,6 +4,8 @@ setenforce 0
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config  
 curl https://get.docker.com|bash
 usermod -aG docker root;systemctl enable docker
+curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 mkdir -p /etc/docker/;wget https://cdn.jsdelivr.net/gh/Subeting/docker-onekey-install/daemon.json -O  /etc/docker/daemon.json
 systemctl daemon-reload
 systemctl restart docker
